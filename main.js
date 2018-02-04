dd3f4512c508ed6eb686e32d9c4a3fbe3fa29606
S Fujimoto <unicoshun@gmail.com>
2018-02-04 16:18:24 +0900

    show one snake in middle

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 const S = 20;
 let W, H;
 let ctx;
+const snake = [];
 window.onload = init;
 
 
@@ -19,6 +20,10 @@ function init() {
   H = canvas.clientHeight / S;
   ctx = canvas.getContext('2d');
   ctx.font = "20px Arial";
+  
+  // 蛇を初期化
+  snake.push(new Point(W / 2, H / 2));
+  paint(); // snakeを描画
 }
 
 //add food
@@ -27,7 +32,7 @@ function addFood() {
 
 // collision
 function isHit(data, x, y) {
-  
+
 }
 
 
@@ -38,7 +43,11 @@ function tick() {
 }
 
 function paint() {
-
+  ctx.fillStyle = "rgb(255, 255, 255)"; // white
+  snake.forEach(function (p) {
+    ctx.fillText("*", p.x * S, (p.y + 1) * S);
+  });
+  
 }
 
 function keydown(event) {
