39aa9833c50879e482f77b74beb8743950b51aca
S Fujimoto <unicoshun@gmail.com>
2018-02-04 16:36:50 +0900

    add tick / setInterval to show addfood()

--- main.js
+++ main.js
@@ -2,6 +2,8 @@ const S = 20;
 let W, H;
 let ctx;
 const snake = [];
+const food = [];
+let timer = NaN;
 window.onload = init;
 
 
@@ -23,23 +25,31 @@ function init() {
   
   // 蛇を初期化
   snake.push(new Point(W / 2, H / 2));
-  paint(); // snakeを描画
+  
+  timer = setInterval("tick()", 200);
+  
 }
 
 //add food
 function addFood() {
+  let x = Math.floor(Math.random() * W);
+  let y = Math.floor(Math.random() * H);
+  food.push(new Point(x, y));
 }
 
 // collision
 function isHit(data, x, y) {
-
+  
 }
 
 
 function moveFood(x, y) {
 }
 
+// フレームごとに実行されるfunction
 function tick() {
+  addFood();
+  paint();
 }
 
 function paint() {
@@ -48,7 +58,12 @@ function paint() {
     ctx.fillText("*", p.x * S, (p.y + 1) * S);
   });
   
+  ctx.fillStyle = "rgb(255, 100, 100)"; // white
+  food.forEach(function (p) {
+    ctx.fillText("*", p.x * S, (p.y + 1) * S);
+  });
 }
+
 
 function keydown(event) {
 }
